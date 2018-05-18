6575b62e837957d1886675eba61e1b96c5c691fa
Glen Walker <glen@walker.gen.nz>
2018-05-18 16:42:01 +1200

    PEP302 compliance experiment

--- src/apipkg/__init__.py
+++ src/apipkg/__init__.py
@@ -42,25 +42,29 @@ def initpkg(pkgname, exportdefs, attr=None, eager=False):
     attr = attr or {}
     oldmod = sys.modules.get(pkgname)
     d = {}
-    f = getattr(oldmod, '__file__', None)
-    if f:
-        f = _py_abspath(f)
-    d['__file__'] = f
-    if hasattr(oldmod, '__version__'):
-        d['__version__'] = oldmod.__version__
-    if hasattr(oldmod, '__loader__'):
-        d['__loader__'] = oldmod.__loader__
+    for name in ('__file__', '__cached__'):
+        try::
+            f = getattr(oldmod, name)
+        except AttributeError:
+            pass
+        else:
+            if f:
+                f = _py_abspath(f)
+            d[name] = f
+    for name in ('__version__', '__loader__', '__package__', '__spec__'):
+        try:
+            d[name] = getattr(oldmod, name)
+        except AttributeError:
+            pass
     if hasattr(oldmod, '__path__'):
         if oldmod.__path__:
             d['__path__'] = [_py_abspath(p) for p in oldmod.__path__]
         else:
             d['__path__'] = oldmod.__path__
-    if hasattr(oldmod, '__package__'):
-        d['__package__'] = oldmod.__package__
     if '__doc__' not in exportdefs and getattr(oldmod, '__doc__', None):
         d['__doc__'] = oldmod.__doc__
     d.update(attr)
-    if hasattr(oldmod, "__dict__"):
+    if hasattr(oldmod, '__dict__'):
         oldmod.__dict__.update(d)
     mod = ApiModule(pkgname, exportdefs, implprefix=pkgname, attr=d)
     sys.modules[pkgname] = mod
